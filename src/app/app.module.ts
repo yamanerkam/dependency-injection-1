64fd86fe9b9b32c29daffe8c96b8bb5703580a73
yamanerkam <erkamyaman35@gmail.com>
2024-10-26 16:10:09 +0300

    injectable-decorator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductService } from './productService';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { ProductService } from './productService';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
